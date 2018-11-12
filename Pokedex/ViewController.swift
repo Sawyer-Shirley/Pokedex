02354b4f30f0ed36db7ae15de2d5a40cd64e5071
Sawyer Shirley <sawyershirley@MacBook-Pro-107.local>
2018-11-12 10:20:07 -0600

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Sawyer Shirley on 11/12/18.
+//  Copyright © 2018 Sawyer Shirley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
